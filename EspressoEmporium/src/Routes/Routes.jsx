6831affebdcc9ed6cd80d936cec7ffa32358024f
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2024-11-26 12:54:30 +0600

    Setup Routes.

--- EspressoEmporium/src/Routes/Routes.jsx
+++ EspressoEmporium/src/Routes/Routes.jsx
@@ -1,11 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
+import ErrorPage from "../Pages/ErrorPage";
+import Home from "../Pages/Home";
+import UpdateCoffee from "../Pages/UpdateCoffee";
+import AddCoffee from "../Pages/AddCoffee";
 
 export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <MainLayout></MainLayout>
-    },
-  ]);
-
-
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/update-coffee',
+        element: <UpdateCoffee />
+      },
+      {
+        path: '/add-coffee',
+        element: <AddCoffee />
+      },
+    ]
+  },
+]);
