2edd832c84a922ca475be09431db265916721655
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2024-11-29 12:49:36 +0600

    Connect DB with Local MongoDBCompass.

--- EmporiumServer/index.js
+++ EmporiumServer/index.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// const uri = "mongodb://localhost:27017";
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.im1kw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
